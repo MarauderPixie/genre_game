f90d0c7037bff9dcd23fe5da202e8173fc1b1aa7
Lukas Burk <lukas@quantenbrot.de>
2019-02-15 16:02:25 +0100

    Reorganize ui/server/global code into files

--- server.R
+++ server.R
@@ -1,118 +1,13 @@
-library(dplyr)
-library(shiny)
-# library(shinyWidgets)
-library(shinyjs)
+# Server definition ----
 
-songs <- readRDS("data/songs.rds") %>% 
-  mutate(
-    title = stringr::str_to_title(title)
-  )
-
-## function to... generate session data
-generate_session <- function() {
-  uid <- paste0(c(sample(LETTERS, 3), "_" ,sample(0:9, 9)), collapse = "")
-  ptf <- paste0("data/", uid, ".rds")
-  
-  smpl <- songs %>% 
-    group_by(genre) %>% 
-    sample_n(4) %>% 
-    ungroup() %>% 
-    mutate(
-      user  = uid,
-      guess = NA
-    )
-  
-  smpl <- smpl[sample(nrow(smpl)),]
-  
-  # saveRDS(smpl, ptf)
-  return(smpl)
-}
-
-
-buttons <- c("count", "hphp", "pop", "metal", "rock")
-
-# Define UI for application ----
-ui <- fluidPage(
-  useShinyjs(),
-  theme = shinythemes::shinytheme("sandstone"),
-  
-  # Application title
-  h1("Can you tell the genre of a song by it's name?", align = "center"),
-  hr(),
-  
-  ### welcome page ----
-  fluidRow(id = "welcome", align = "center",
-    column(width = 10, offset = 1,
-      p("Most people have strong opinions about their favorite or least favorite musical genre or even
-           about genres in general. One way or another, music is often a strong identification device. 
-           But how different are musical genres actually in terms of language? How well are people able
-           to distinguish between them? Let's find out!"), 
-      br(),
-      p("On the following pages, you will be presented a title of a song and we ask you to tell us what
-           you think which genre that title comes from. There will be a total of 20 songs and afterwards
-           you will get your results: on how many have you been correct, on which ones and what genre
-           does it actually belong to?"), 
-      br(),
-      p("No data other than your responses and a unique number will be stored. In no way will it be
-           possible to identify you at any point during or after the session. You're participation is 
-           completely voluntary."), 
-      br(),
-      p("Thanks for your help and have fun!"), 
-      br(),
-      actionButton("start", "Let's do it!")
-    )
-  ),
-  
-  ## questioning page ----
-  hidden(
-    fluidRow(id = "questionpage", align = "center",
-      column(10, offset = 1,
-      
-      # I'll leave div-id "song01" in for now, in case I come up with a
-      # shinier version to display the title
-        p("What's the genre of"),
-        h2(id = "song01", textOutput("song_number")),
-        p("?"),
-        
-        actionButton("count", "Country"),
-        actionButton("hphp", "Hip-Hop"),
-        actionButton("pop", "Pop"),
-        actionButton("metal", "Metal"),
-        actionButton("rock", "Rock")
-      )
-    )
-  ),
-  
-  ## results page ----
-  hidden(
-    fluidRow(id = "results", align = "center",
-      column(width = 10, offset = 1,
-      
-        h2("You're done. Here are your results:"),
-
-        h3(textOutput("perc")),
-        
-        p("here's a detailed table for you."),
-        
-        tableOutput("result_table"),
-        
-        p("Feel free to close the browser window anytime now.")
-      )
-    )
-  )
-)
-
-
-
-# Define server logic ----
-server <- function(input, output, session) {
+shinyServer(function(input, output, session) {
   
   smpl <- reactive({
     input$start
     generate_session()
   })
   
-
+  
   cnt <- reactive({
     input$count
     if (input$count == 0) return(data.frame())
@@ -153,7 +48,7 @@ server <- function(input, output, session) {
     guesses <- rbind(cnt(), hip(), pop(), mtl(), rck())
     guesses[guesses$timestamp == max(guesses$timestamp), "genre"]
   })
-
+  
   
   
   # count button clicks, switch song and eventually pages
@@ -167,7 +62,7 @@ server <- function(input, output, session) {
     #                                   "Pop:    ", input$pop, "\n",
     #                                   "Metal:  ", input$metal, "\n",
     #                                   "Rock:   ", input$rock))
-
+    
     if (i() > 1 & i() <= 21) {
       cat(
         paste(smpl()$user[1], ',"',
@@ -216,8 +111,4 @@ server <- function(input, output, session) {
       })
     }
   })
-}
-
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+})
