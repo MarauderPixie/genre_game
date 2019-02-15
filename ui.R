8dfcaa1ab8600a8a952dc32435d6a7a696b6aa1e
MarauderPixie <tobias.anton.85@gmail.com>
2019-02-15 19:26:38 +0100

    update intro text; add nickname field

--- ui.R
+++ ui.R
@@ -13,20 +13,24 @@ shinyUI(
     fluidRow(id = "welcome", align = "center",
              column(width = 10, offset = 1,
                     p("Most people have strong opinions about their favorite or least favorite musical genre or even
-           about genres in general. One way or another, music is often a strong identification device. 
+           about genres in general. One way or another, music is something manly people strongly identify with. 
            But how different are musical genres actually in terms of language? How well are people able
-           to distinguish between them? Let's find out!"), 
+           to distinguish between these differences? Let's find out!"), 
                     br(),
-                    p("On the following pages, you will be presented a title of a song and we ask you to tell us what
-           you think which genre that title comes from. There will be a total of 20 songs and afterwards
+                    p("On the following pages, you will be presented a title of a song and five genres and you select
+           the one you think that title belongs to. There will be a total of 20 songs. Afterwards
            you will get your results: on how many have you been correct, on which ones and what genre
            does it actually belong to?"), 
                     br(),
                     p("No data other than your responses and a unique number will be stored. In no way will it be
            possible to identify you at any point during or after the session. You're participation is 
            completely voluntary."), 
+                    p("Only if you want to you can enter a nickname in the field below so you can brag about your
+           exceptional knowledge and precision - should you make it into the leaderboard."),
                     br(),
-                    p("Thanks for your help and have fun!"), 
+                    textInput("nickname", "Your nickname:", placeholder = "leave emty if not interested"), 
+                    br(),
+                    p("Thanks for taking part and have fun!"), 
                     br(),
                     actionButton("start", "Let's do it!")
              )
